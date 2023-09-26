78e92a97993650cd8c446785a2573f698aa76692
tovantich0028 <vantich12a6@gmail.com>
2023-09-26 19:56:27 +0700

    add

--- WebsiteBanHang/Controllers/HomeController.cs
+++ WebsiteBanHang/Controllers/HomeController.cs
@@ -3,28 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanHang.Context;
 
 namespace WebsiteBanHang.Controllers
 {
     public class HomeController : Controller
     {
+        ShopModelEntities objModel = new ShopModelEntities();
         public ActionResult Index()
         {
+            var lstCategory = objModel.Categories.ToList();
+            var lstProduct = objModel.Products.ToList();
+           
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
+        
     }
 }
